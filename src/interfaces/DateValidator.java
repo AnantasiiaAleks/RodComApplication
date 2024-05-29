50181201b9f7a61f92ed3457a9919475217b141b
AnastasiiaAleks <chiffka@gmail.com>
2024-05-29 11:38:39 +0300

    в абстрактном классе Person прописан метод addNewPerson, а также геттеры и сеттеры. Добавлен интерфейс проверки валидности даты

---
+++ src/interfaces/DateValidator.java
@@ -0,0 +1,16 @@
+package interfaces;
+
+import java.text.SimpleDateFormat;
+
+public interface DateValidator {
+    default boolean isValid(String dateStr) {
+        SimpleDateFormat myFormat = new SimpleDateFormat("dd.MM.yyyy");
+        myFormat.setLenient(false);
+        try {
+            myFormat.parse(dateStr);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
--- src/person/Person.java
+++ src/person/Person.java
@@ -1,21 +1,73 @@
 package person;
 
+import interfaces.InputFromUser;
+
 public abstract class Person {
     private String firstName;
     private String lastName;
-    private String patronymic;
+    private String middleName;
     private String telNumber;
     private String birthDate;
 
     public Person (String firstName, String lastName,
-                   String patronymic, String telNumber, String birthDate) {
+                   String middleName, String telNumber, String birthDate) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.patronymic = patronymic;
+        this.middleName = middleName;
         this.telNumber = telNumber;
         this.birthDate = birthDate;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getTelNumber() {
+        return telNumber;
+    }
+
+    public void setTelNumber(String telNumber) {
+        this.telNumber = telNumber;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
     public String toString() {
-        return firstName + " " + lastName + " " + patronymic + " " + birthDate + " " + telNumber;
+        return firstName + " " + lastName + " " + middleName + " " + birthDate + " " + telNumber;
+    }
+
+    public void AddNewPerson(){
+        lastName = InputFromUser.inputText("Фамилия: ");
+        firstName = InputFromUser.inputText("Имя: ");
+        middleName = InputFromUser.inputText("Отчество: ");
+        birthDate = InputFromUser.inputText("Дата рождения: ");
+        telNumber = InputFromUser.inputText("Номер телефона: ");
+
     }
 }
